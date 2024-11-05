692d5107bed8843debbf76517fa690bbb5cfe5bf
spidey simp <michael.lapio@gmail.com>
2024-11-05 13:05:42 -0800

    1.0.1.1
    Minor formatting change. Nothing major was updated.

--- swgohtools/swgohtools.py
+++ swgohtools/swgohtools.py
@@ -68,43 +68,43 @@ class SwgohTools(commands.Cog):
     async def swgohtools_tbstarset(self, ctx: commands.Context, star: int = 0):
         """Sets the amount of stars your guild gets in tb. - Only supports rote stars right now."""
         await self.config.user(ctx.author).tbstar.set(star)
-        await ctx.send(f"Your territory battle stars are now set to {star} stars")
+        await ctx.send(f"```Your territory battle stars are now set to {star} stars```")
 
     @swgohtools.command(name="getspending", aliases=["gs"])
     async def swgohtools_getspending(self, ctx: commands.Context, geton: bool = True):
         """Toggles if you are using guild event tokens toward kyro spending. Add either true or false as a parameter."""
         await self.config.user(ctx.author).getcurrency.set(geton)
-        await ctx.send(f"GET currency spending on kyros is now set to {geton}")
+        await ctx.send(f"```GET currency spending on kyros is now set to {geton}```")
 
     @swgohtools.command(name="zeffo", aliases=["z"])
     async def swgohtools_zeffoset(self, ctx: commands.Context, zeffoon: bool = True):
         """Toggles if you have zeffo unlocked."""
         await self.config.user(ctx.author).zeffo.set(zeffoon)
-        await ctx.send(f"You have set your zeffo completion to: {zeffoon}")
+        await ctx.send(f"```You have set your zeffo completion to: {zeffoon}```")
 
     @swgohtools.command(name="mandalore", aliases=["m"])
     async def swgohtools_mandaloreset(self, ctx: commands.Context, mandalorecompletion: bool = True):
         """Toggles if you have mandalore unlocked. Add either true or false as a parameter."""
         await self.config.user(ctx.author).mandalore.set(mandalorecompletion)
-        await ctx.send(f"You have set your mandalore competion to: {mandalorecompletion}")
+        await ctx.send(f"```You have set your mandalore competion to: {mandalorecompletion}```")
     
     @swgohtools.command(name="twgp")
     async def swgohtools_twgpset(self, ctx: commands.Context, twgpnumber: int = 0):
         """Sets the active territory war gp that your guild has. Input just what x would be in the following expression: tw gp = x million"""
         await self.config.user(ctx.author).twgp.set(twgpnumber)
-        await ctx.send(f"Your active territory war gp is now {twgpnumber} million.")
+        await ctx.send(f"```Your active territory war gp is now {twgpnumber} million.```")
 
     @swgohtools.command(name="energyspent", aliases=["es"])
     async def swgohtools_kyroenergy(self, ctx: commands.Context, energyamt: int = 0):
         """Sets how much energy you spend per day on kyros."""
         await self.config.user(ctx.author).energy.set(energyamt)
-        await ctx.send(f"You are now spending {energyamt} campaign energy on kyros per day.")
+        await ctx.send(f"```You are now spending {energyamt} campaign energy on kyros per day.```")
     
     @swgohtools.command(name="abcompletion", aliases=["abc"])
     async def swgohtools_abcompletion(self, ctx: commands.Context, abamt: int = 0):
         """Sets how many of the Challenge Tier 1s you have completed for assault battles."""
         await self.config.user(ctx.author).abcompletion.set(abamt)
-        await ctx.send(f"You have the setting of {abamt} assault battles completed.")
+        await ctx.send(f"```You have the setting of {abamt} assault battles completed.```")
 
     @swgohtools.command(name="kyrocalc", aliases=["kc"])
     async def swgohtools_kyrocalc(self, ctx: commands.Context):
@@ -249,14 +249,14 @@ class SwgohTools(commands.Cog):
 
         totalkyro = tbbox + getrewards + zefforewards + mandalorerewards + twrewards + campenergy + abkyros
         await ctx.send(
-            f"Here is your kyro count:\nThe total amount of kyros you'll get in a month is: {totalkyro}"
+            f"```Here is your kyro count:\nThe total amount of kyros you'll get in a month is: {totalkyro}"
             f"\nThe amount of kyros you'll get from the territory battle reward boxes is: {tbbox}"
             f"\nThe amount of kyros you'll get from spending territory rewards is: {getrewards}"
             f"\nThe amount of kyros you'll get from zeffo is: {zefforewards}"
             f"\nThe amount of kyros you'll get from mandalore is: {mandalorerewards}"
             f"\nThe amount of kyros you'll get from territory war reward boxes is: {twrewards}"
             f"\nThe amount of kyros you'll get from energy is an average of: {campenergy}"
-            f"\nThe amount of kyros you'll get from assault battles is: {abkyros}"
+            f"\nThe amount of kyros you'll get from assault battles is: {abkyros}```"
         )
 
     
@@ -271,11 +271,11 @@ class SwgohTools(commands.Cog):
         energy = await self.config.user(ctx.author).energy()
         abcompletion = await self.config.user(ctx.author).abcompletion()
         await ctx.send(
-            f"Your current swgoh settings are set to:\nTerritory Battle Star count - {tbstar} stars"
+            f"```Your current swgoh settings are set to:\nTerritory Battle Star count - {tbstar} stars"
             f"\nGet spending on Kyros - {getcurrency}"
             f"\nYour guild's zeffo unlock status is - {zeffo}"
             f"\nYour guild's mandalore unlock status is - {mandalore}"
             f"\nYou have the following active Territory War gp - {twgp} million"
             f"\nYou are spending {energy} energy on kyros per day (on average)."
-            f"\nYou have completed the Challenge Tier 1 of {abcompletion} assault battles so far."
+            f"\nYou have completed the Challenge Tier 1 of {abcompletion} assault battles so far.```"
             )
