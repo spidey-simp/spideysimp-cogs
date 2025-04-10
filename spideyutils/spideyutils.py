07cbf7c8c7a4b9db1f8552ef9c77117bc8d1ed33
spidey-simp <michael.lapio@gmail.com>
2025-04-10 09:22:13 -0700

    added leader element

--- spideyutils/spideyutils.py
+++ spideyutils/spideyutils.py
@@ -297,10 +297,6 @@ class SpideyUtils(commands.Cog):
             overview.add_field(name=self.classify_stamp(knowledge), value=self.general_report(country, knowledge), inline=False)
         overview.description = self.redacted(target.get("details", "No description."), knowledge)
         overview.set_footer(text=footer_text)
-        if leader.get("image"):
-            overview.set_author(name=f"Leader: {leader.get('name', '[REDACTED]')}", icon_url=leader["image"])
-        else:
-            overview.set_author(name=f"Leader: {self.redacted(leader.get('name', 'Unknown'), knowledge)}")
         if target.get("image"):
             overview.set_image(url=target["image"])
         ideology = target.get("ideology", {})
@@ -308,6 +304,23 @@ class SpideyUtils(commands.Cog):
         overview.add_field(name="Doctrine", value=target.get("global", {}).get("doctrine_focus", "N/A"), inline=True)
         overview.add_field(name="Conscription", value=target.get("global", {}).get("conscription_policy", "N/A"), inline=True)
         embeds.append(overview)
+
+        # LEADER
+        leader_name = leader.get("name", "Unknown")
+        leader_desc = []
+        if not is_owner:
+            leader_desc.append("// EYES ONLY - DO NOT DISTRIBUTE //")
+        leader_desc.append(self.redact_paragraph_weighted((leader.get("description"), "No biographical data available."), knowledge))
+        leader_img = leader.get("image", None)
+
+        if leader_name or leader_desc:
+            leader_embed = discord.Embed(
+                title=f"[CONFIDENTIAL] - EVAL. OF {leader_name.upper()}" if not is_owner else f"{leader_name} Profile",
+                description=f"{leader_desc}"
+            )
+            if leader_img:
+                leader_embed.set_image(url=leader_img)
+            embeds.append(leader_embed)
 
         # MILITARY
         m = target.get("military", {})
@@ -383,7 +396,7 @@ class SpideyUtils(commands.Cog):
                 for spirit in spirits:
                     name = self.redacted(spirit.get("name", "Unknown"), knowledge)
                     desc = self.redact_paragraph_weighted(spirit.get("description", "No description available."), knowledge)
-                    embed.add_field(name=f"🕊️ {name}", value=desc, inline=False)
+                    embed.add_field(name=f"**{name}**", value=desc, inline=False)
 
                 embeds.append(embed)
 
