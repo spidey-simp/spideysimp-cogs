b5953b0c903e1974e8d4d63d2202fc9d0ebd6f37
spidey-simp <michael.lapio@gmail.com>
2025-05-24 02:56:19 -0700

    hotfix

--- languify/languify.py
+++ languify/languify.py
@@ -2,7 +2,6 @@ import aiohttp
 from redbot.core import commands, Config
 import discord
 from discord import app_commands
-from discord.ext import commands
 import random
 
 ACCEPTED_LANGUAGES = ["pirate"]
