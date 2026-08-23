3d96e2e14bee575468fca70f03c0c5edc24b46f4
spidey-simp <michael.lapio@gmail.com>
2026-08-23 10:47:52 -1000

    feat: Refactor Hoi4Random cog and update info.json for improved functionality and clarity

--- hoi4random/hoi4random.py
+++ hoi4random/hoi4random.py
@@ -1,21 +1,14 @@
 from __future__ import annotations
-import discord
-import logging
-import random
-import aiohttp
-from discord import app_commands
 
-from redbot.core import Config, checks, commands
-from redbot.core.bot import Red
+import random
+
+import discord
+from discord import app_commands
+from redbot.core import commands
 
 from .hoi4leaderlist import COUNTRYLIST, HISTORICAL_EMPIRES
 
 
-async def fetch_url(session, url):
-    async with session.get(url) as response:
-        assert response.status == 200
-        return await response.json()
-    
 
 alignment = [
     "Democratic",
@@ -28,9 +21,7 @@ class Hoi4Random(commands.Cog):
     """Pick a random Hoi 4 leader using the following commands!"""
 
     def __init__(self, bot):
-        super().__init__()
         self.bot = bot
-        self.config = Config.get_conf(self, identifier=684457913250480143, force_registration=True)
         
 
     async def red_delete_data_for_user(self, **kwargs):
@@ -46,6 +37,7 @@ class Hoi4Random(commands.Cog):
         await interaction.response.send_message(f"```The full HOI 4 Leader list is:\n- {indexseparator.join(COUNTRYLIST.keys())}```")
 
     @hoi4random.command(name="random", description="Get a random leader from the full list.")
+    @app_commands.checks.bot_has_permissions(embed_links=True)
     async def random(self, interaction: discord.Interaction):
         """Get a random leader selected from the full list."""
         civtitle = "Your Hearts of Iron IV leader generation has generated:"
@@ -59,11 +51,12 @@ class Hoi4Random(commands.Cog):
         em = discord.Embed(
             title=civtitle, description=civresult, color=discord.Color.red()
         )
-        
-        em.set_image(url=civimage)
+        if civimage:
+            em.set_image(url=civimage)
         await interaction.response.send_message(embed=em)
 
     @hoi4random.command(name="empire_form", description="Get a random Empire to form.")
+    @app_commands.checks.bot_has_permissions(embed_links=True)
     async def empire_form(self, interaction: discord.Interaction):
         """Get a random Empire to form."""
         empiretitle = "Your Hearts of Iron IV Empire generation has generated:"
@@ -72,16 +65,11 @@ class Hoi4Random(commands.Cog):
         empire_description = empire_info.get("description", "No description available.")
         empire_image = empire_info.get("image", None)
 
-        async with aiohttp.ClientSession(headers={"Connection": "keep-alive"}) as session:
-            async with session.get(empire_image, ssl=False) as response:
-                if response.status != 200:
-                    await interaction.response.send_message(
-                        "There was an error fetching the image. Please try again later.", ephemeral=True
-                    )
-                    return
+
 
         em = discord.Embed(
             title=empiretitle, description=empire_description, color=discord.Color.red()
         )
-        em.set_image(url=empire_image)
+        if empire_image:
+            em.set_image(url=empire_image)
         await interaction.response.send_message(embed=em)
